00ee4ce35b02675fdc60a551b0f0fd98dfb33a63
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2026-07-28 13:21:45 -0400

    Update versioning

--- serviceWorker.js
+++ serviceWorker.js
@@ -39,7 +39,7 @@ registerRoute(
 );
 
 workbox.precaching.precacheAndRoute([
-        {url: 'index.html', revision: 'v1.71'},
-        {url: 'src/pages/dataRequest.js', revision: 'v1.71'},
+        {url: 'index.html', revision: 'v1.72'},
+        {url: 'src/pages/dataRequest.js', revision: 'v1.72'},
     ]
 );
